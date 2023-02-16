4235e34bb0267783c0aa8b077e914b66d39cfefe
MikeR20183380 <20183380@student.hhs.nl>
2023-02-16 23:47:27 +0100

    Deur Optimise
    Deur geoptimaliseerd

--- Wemos/DeurHerkans/DeurHerkans.ino
+++ Wemos/DeurHerkans/DeurHerkans.ino
@@ -3,180 +3,197 @@
 #include <Servo.h>
 
 #ifndef WIFISSID
-#define WIFISSID "BeamersWifi" // Wifi naam
-#define WIFIPASS "112233ja"  // Wachtwoord van de wifi
+#define WIFISSID "BeamersWifi"  // Wifi network name.
+#define WIFIPASS "112233ja"     // Password of the wifi network.
 #endif
 
 const char* ssid = WIFISSID;
 const char* password = WIFIPASS;
-const char* host = "192.168.137.10";  //ip van de pi, kan ook pi@mikePi.local zijn
+const char* host = "192.168.137.10";  //Ip of the Pi, can also be pi@mikePi.local.
 const uint16_t port = 8080;
 
-Servo myservo;
+Servo myservo;              // Initialize a servo variable.
+WiFiClient client;          // Initialize a wificlient to use for making connections.
 
-int deurdicht = 80;  // deur sluiten
-int deuropenin = 170;  // deur openen naar binnen zijde
-int deuropenuit = 0; // deur openen naar buiten zijde
+// Angles for servo
+int deurdicht = 80;
+int deuropenin = 170;
+int deuropenuit = 0;
 
 void setup() {
-  myservo.attach(D5);    // geef aan op welke pin de servo zit
-  deurSluiten();         // deur sluiten wanneer device gestart wordt
-  Serial.begin(115200);  // serieel starten met de juiste baudrate
-  Wire.begin();          // I2C communicatie beginnen
-  wifiVerbinden();       // wifi verbinden aanroepen, deze zorgt ervoor dat we gaan verbinden met de juiste server.
+  myservo.attach(D5);       // Assign pin D5 to be the servo pin.
+  closeDoor();              // Close the door on program startup.
+  Serial.begin(115200);     // ================ serieel starten met de juiste baudrate.
+  Wire.begin();             // I2C communicatie beginnen.
+}
+
+void delaySeconds(int seconds) {
+  for (int i = 0; i < seconds; i++) {
+    delay(1000);
+  } 
 }
 
 void loop() {
-  bool brand = false;
-  bool hulp = false;
-  WiFiClient client = verbindenPi();  // verbinding maken met de host/server Pi
-  if(client.connected()){
-    client.print("setDeur");
+  // Check if a wifi connection is currently active, otherwise try to establish one.
+  if (WiFi.status() != WL_CONNECTED) {
+    connectToWifi();
+    return;
   }
-  int timer1ms = 0;
-  int vorigeKnopjes = 999;
 
-  while (1) {
-    int knopjes = knopjesLezen();
-    if (knopjes != vorigeKnopjes) {
-      Serial.print("Waarde knopjes:");
-      Serial.println(knopjes);
+  // Try to make a connection to the pi server. If no success, reset.
+  if (!connectToServer())
+    return;
+  
+  // Set server configuration to match door usage. 
+  Serial.print("\nSetting deur to server");
+  client.print("setDeur");  // ENGLISH =============
+
+  bool hulp = false;
+  int previousInput = 999;
+
+  /*  
+    Start input reading sequence in a while loop,
+    testing on client.connected() to boot code out of the loop when connection gets lost.
+  */
+  while (client.connected()) {
+    int input = readInput();
+    // Serial.print("\nInput : ");
+    // Serial.print(input);
+    // Serial.print("\PreviousInput : ");
+    // Serial.print(previousInput);
+    
+    if (input != 0 && input == previousInput) {
+      
+      Serial.print("\nSame input detected, skipping...");
+      delay(1000);
+      continue;
     }
-    vorigeKnopjes = knopjes;
+    
+    // Set previousInput to the input received on previous cycle, unless its 0 meaning no input, to prevent input spam.
+    if (input != previousInput) {
+      previousInput = input;
+    }
 
     if (client.connected()) {
-      if (knopjes == 1) {
+      if (input == 1) {
         client.print("deurOpenBinnen");
-        delay(300);
+        delay(1000); // Depends on wifi speed
       }
-      if (knopjes > 1) {
+      if (input == 2) {
         client.print("deurOpenBuiten");
-        delay(300);
+        delay(1000); // Depends on wifi speed
       }
-      knopjes = 0;
     }
 
     if (client.available()) {  // Lezen wat de server verstuurt en printen.
-      char ch = static_cast<const char>(client.read());
-      Serial.print("pi zegt:");
-      Serial.print(ch);
-      Serial.print("\n");
+      char message = static_cast<const char>(client.read());
 
-      if (ch == '1' && !brand) { // Pi stuurt dat deur geopend moet worden
-        deurOpenen(brand);
-        Serial.println("Open deur");
-      }
-      if (ch == '8') { // Pi stuurt dat er brand is
-        brand = true;
-        deurOpenen(brand);
-        Serial.println("Brand!!");
-      }
-      if (ch == '9') { // Pi stuurt dat de brand voorbij is.
-        brand = false;
-        deurOpenen(brand);
-        Serial.println("Geen brand!");
-      }
-      if (ch == '4') { // Pi stuurt dat patiënt hulp nodig heeft
-        hulp = true;
-        Serial.println("Help patiënt!!");
-      }
-      if (ch == '5') { // Pi stuurt dat patiënt geen hulp meer nodig heeft
-        hulp = false;
-        Serial.println("Patiënt geholpen :)");
-      }
-      ch = '0';  // zodat deur niet dubbel commando's gaat doen. voor het geval dat.
-    }
+      // Print callback from server
+      Serial.print("\nPi zegt: ");
+      Serial.print(message);
 
-    if (!client.connected()) {
-      Serial.println("Verbinding met Server (Pi) verbroken");
-      break; // zodat wemos opnieuw verbindt met de Pi
-    }
-    if ((WiFi.status() != WL_CONNECTED)) {
-      Serial.println("Verbinding met WiFi verbroken");
-      setup(); // zodat wemos opnieuw verbindt tot het access-point
-    }
-    delay(1); 
-    if (!brand && hulp) {
-      if (timer1ms < 1001) {
-        timer1ms++;
+      switch (message) {  
+        case '1':
+          // Open door inwards
+          Serial.print("\nOpen deur naar binnen");
+          doorController(1);
+          Serial.print("\nDeur gaat dicht");
+          previousInput = 0;
+          break;
+
+        case '2':
+          // Open door outwards
+          Serial.print("\nOpen deur naar buiten");
+          doorController(2);
+          Serial.print("\nDeur gaat dicht");
+          previousInput = 0;
+          break;
+
+        case '4':
+          // Patiënt requires help
+          hulp = true;
+          Serial.print("\nHelp patiënt!!");
+          break;
+
+        case '5':
+          // Patiënt no longer requires help
+          hulp = false;
+          Serial.print("\nPatiënt geholpen :)");
+          break;
+
+        default:
+          // Empty input
+          break;          
       }
-      if (timer1ms > 499) {
-        lichtAan();
-      }
-      if (timer1ms > 998) {
-        timer1ms = 0;
-        lichtUit();
-      }
     }
+    continue;
+  }
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("\nConnection to wifi network lost");
+    return;
+  } else {
+    Serial.print("\nConnection to server (Pi) lost");
+    return;
   }
 }
 
-void deurOpenen(bool brandje) {  // pi kan deze functie aanroepen om deur te openen
-  lichtAan(); // licht aan want praktisch voor patiënt
-  myservo.write(deuropenin); // servo opent de deur
-  int knop;
-  for (int i = 0; i < 500; i++) {
-    int knop = knopjesLezen();
-    if (knop > 0) { // > 0 betekent dat een knop is ingedrukt
+// Attempting to make a connection to wifi network from Pi
+void connectToWifi() {                     
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  Serial.print("\nEstablishing Connection to wifi network");
+  for (int i = 0; i < 10; i++) {
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.print(".");
+      delay(1000);
+    } else {
+      Serial.print("\nWiFi connection established on " + WiFi.localIP().toString());
       return;
     }
-    delay(10);
-  }
-  //delay(50);
-  if (!brandje) {
-    deurSluiten();  // deur blijft 5 seconden open en gaat dan weer dicht.
-  }
+  } 
+  Serial.print("\nCouldn't establish a connection to wifi network");
 }
 
-void deurSluiten() {  // later moet brandvariabele meegegeven worden.
-  lichtUit(); // licht hoeft niet meer aan bij een dichte deur
-  myservo.write(deurdicht);  // servo schrijven naar deur dicht hoek, 80 graden
-}
+bool connectToServer() {
+  Serial.print("\nEstablishing connection to (Pi) server");
 
-void wifiVerbinden() {                     // verbinding maken met Accespoint van de Pi.
-  WiFi.mode(WIFI_STA);                     //
-  WiFi.begin(ssid, password);              // arduino library doet het moeilijke werk
-  while (WiFi.status() != WL_CONNECTED) {  // print . terwijl hij aan t verbinden is.
-    delay(500);
+  if (!client.connect(host, port)) {
+    Serial.print("\nCouldn't establish a connection to (Pi) server on ");
+  }
+
+  for (int i = 0; i < 10; i++) {
+    if (client.connected()) {
+      Serial.print("\n(Pi) Server connection established on ");
+      //Serial.print("" + host + ":" + port.to);
+      return 1;
+    }
     Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+    delay(1000);
+  } 
+  
+  Serial.print("\nCouldn't establish a connection to (Pi) server on ");
+  // Serial.print( + host + ":" + port);
+  return 0;
 }
 
-int knopjesLezen() {
-  //Config PCA9554
-  //Inside loop for debugging purpose (hot plugging wemos module into i/o board).
-  //IO0-IO3 as input, IO4-IO7 as output.
-  Wire.beginTransmission(0x38);  // slave adres meegeven
+int readInput() {
+  // Config PCA9554
+  // Inside loop for debugging purpose (hot plugging wemos module into i/o board).
+  // IO0-IO3 as input, IO4-IO7 as output.
+  Wire.beginTransmission(0x38);       // slave adres meegeven
   Wire.write(byte(0x03));
   Wire.write(byte(0x0F));
   Wire.endTransmission();
 
-  //Read PCA9554 outputs (IO40-IO3)
-  Wire.beginTransmission(0x38);  // slave adres meegeven
+  // Read PCA9554 outputs (IO40-IO3)
+  Wire.beginTransmission(0x38);       // slave adres meegeven
   Wire.write(byte(0x00));
   Wire.endTransmission();
   Wire.requestFrom(0x38, 1);
   unsigned int inputs = Wire.read();  // hier lees je waarde van knopje met I2C
   return inputs & 0x03;               // knopje waarden teruggeven aan de main.
-}
-
-WiFiClient verbindenPi() {  // verbinden met de pi
-  Serial.print("connecting to: ");
-  Serial.print(host);
-  Serial.print(" :");
-  Serial.println(port);
-
-  // Hiermee zet je de Wificlient class in TCP connectie modus.
-  WiFiClient client;  // client aanmaken
-  if (!client.connect(host, port)) {
-    Serial.println("connection failed");
-    delay(5000);
-  }
-  return client;
 }
 
 void lichtAan() {
@@ -193,4 +210,23 @@ void lichtUit() {
   Wire.write(byte(0x01));
   Wire.write(byte(0));
   Wire.endTransmission();
+}
+
+void doorController(int status) {   // pi kan deze functie aanroepen om deur te openen
+  lichtAan();                       // licht aan want praktisch voor patiënt
+
+  if (status == 1) {
+    myservo.write(deuropenin);
+  }
+  else if (status == 2) {
+    myservo.write(deuropenuit);
+  }
+
+  delaySeconds(5);
+  closeDoor();                // deur blijft 5 seconden open en gaat dan weer dicht.
+}
+
+void closeDoor() {            // later moet brandvariabele meegegeven worden.
+  lichtUit();                 // licht hoeft niet meer aan bij een dichte deur
+  myservo.write(deurdicht);   // servo schrijven naar deur dicht hoek, 80 graden
 }
