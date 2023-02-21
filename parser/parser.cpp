098197e0e4dbfdd3854a4641ef6b8df50a61f0f2
Mike <102684812+MikeeRietveldt@users.noreply.github.com>
2023-02-21 18:48:32 +0100

    Update parser.cpp
    Adding comments

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -6,13 +6,13 @@
 
 void Parser::parse(char *buffer)
 {
-    std::string input = buffer;
-    std::istringstream iss(input);
+    std::string input = buffer; //Convert buffer into C++ string.
+    std::istringstream iss(input); //Safe way of dynamically reading other  datatypes (ints, doubles, floats etc.) from a string.
 
     //Add every single word to Vector
-    std::string sub;
-    while(iss >> sub)
+    std::string sub; //We will use this for the While loop.
+    while(iss >> sub) //Keeps going till there are no words left.
     {
-        values.push_back(sub);
+        values.push_back(sub); //Add this at the end of the vector. So we can later retrieve it.
     }
 }
